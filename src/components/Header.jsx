098283293a52e21c17e959bfae20d76f8b2061e0
Paul K <spkim0921@gmail.com>
2024-02-05 13:20:52 -0800

    2nd commit
    home page design update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,18 +10,18 @@ export default function Header() {
 
 
     return (
-        <header className="sticky z-50 top-0 md:flex justify-between">
+        <header className="sticky z-50 top-0 md:flex justify-between py-5 bg-indigo-600 text-white">
             <div>
-                <NavLink to="/">CapyTalk</NavLink>
-                <NavLink to="/">Home</NavLink>
+                <NavLink to="/" className="px-5">CapyTalk</NavLink>
+                <NavLink to="/" className="px-5">Home</NavLink>
             </div>
             <div>
-                <NavLink>About</NavLink>
-                <NavLink>Contact</NavLink>
+                <NavLink className="px-5">About</NavLink>
+                <NavLink className="px-5">Contact</NavLink>
             </div>
             <div>
-                <NavLink to="/users/login">Login</NavLink>
-                <NavLink to="/users/signup">Sign Up</NavLink>
+                <NavLink to="/users/login" className="px-5">Login</NavLink>
+                <NavLink to="/users/signup" className="px-5">Sign Up</NavLink>
             </div>
         </header>
     )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,26 @@
+
+
+import { NavLink } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-
-
+import useAuthStore from "../store/AuthStore";
 
 export default function HomePage() {
 
+    const { user } = useAuthStore((state) => state)
 
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen ">
             <Header />
-            <main className="flex-1 mx-auto">
-                Hello
+            <h1 className="py-5 text-2xl font-medium text-center bg-indigo-600 text-white">CapyTalk</h1>
+            <p className="py-5 font-medium text-center bg-indigo-600 text-white">Chat, communicate, play with friends and have fun</p>
+            <main className="flex-1 mx-auto ">
+            {!user && <div className="flex flex-col items-center">
+                <div className="border rounded-xl py-5 px-5 my-5 bg-indigo-600 text-white">
+                    <NavLink to="/users/login">Login</NavLink>
+                </div>
+                <NavLink to="/users/signup" className="underline">Sign Up</NavLink>
+            </div>}
             </main>
             <Footer />
         </div>
